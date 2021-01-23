afc486a213ee61d4e873077bda662688cfbaec0f
MCTzOCK <dev@mctzock.de>
2021-01-23 08:57:12 +0100

    Added NMS Dependency

--- src/main/java/net/craftions/lasertag/Lasertag.java
+++ src/main/java/net/craftions/lasertag/Lasertag.java
@@ -4,10 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Lasertag extends JavaPlugin {
 
+    public static Lasertag plugin;
+    public static String prefix = "[§4LaserTag§r] ";
+
     @Override
     public void onEnable() {
+        plugin = this;
         // Plugin startup logic
-
+        System.out.println("Loaded LaserTag!");
     }
 
     @Override

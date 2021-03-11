84fe177dfa90c8e0ba53ca7abb9ffb03af889505
Sausefix <dev@craftions.net>
2021-03-11 19:15:21 +0100

    collect event cancelled fixed

--- src/main/java/net/craftions/lasertag/Lasertag.java
+++ src/main/java/net/craftions/lasertag/Lasertag.java
@@ -3,6 +3,7 @@ package net.craftions.lasertag;
 import net.craftions.lasertag.events.*;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -20,6 +21,7 @@ public final class Lasertag extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new EventEntityDamage(), this);
         Bukkit.getPluginManager().registerEvents(new EventPlayerDeath(), this);
         Bukkit.getPluginManager().registerEvents(new EventPlayerRespawn(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerPickupItemEvent, this);
         System.out.println("Loaded LaserTag!");
     }
 

a7fdb3ff7dfc0c583f353cf14724005b1d1e8dd4
Sausefix <dev@craftions.net>
2021-03-11 19:29:58 +0100

    canceled dropevent fixed (fixed)

--- src/main/java/net/craftions/lasertag/events/EventEntityDrop.java
+++ src/main/java/net/craftions/lasertag/events/EventEntityDrop.java
@@ -1,13 +1,14 @@
 package net.craftions.lasertag.events;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class EventEntityDrop implements Listener {
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent t) {
 
-    public void onDrop(PlayerDropItemEvent t){
-
-    t.setCancelled(true);
+        t.setCancelled(true);
 
 
     }
